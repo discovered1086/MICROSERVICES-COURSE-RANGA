369a61cdd286695e05fb647215864d300bc72849
Kingshuk Mukherjee <45887761+discovered1086@users.noreply.github.com>
2022-06-04 19:30:24 -0500

    Removing all projects to start fresh

--- currency-exchange-service/src/main/java/com/kingshuk/springcloudprojects/currencyexchangeservice/dao/ExchangeValueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kingshuk.springcloudprojects.currencyexchangeservice.dao;
-
-import com.kingshuk.springcloudprojects.currencyexchangeservice.model.ExchangeValue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
-
-    ExchangeValue findByFromCurrencyAndToCurrency(String fromCurrency, String toCurrency);
-}
