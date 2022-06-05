7d515dcff8a762000305be2dde216f184c9e596c
Kingshuk Mukherjee <45887761+discovered1086@users.noreply.github.com>
2022-06-04 21:27:24 -0500

    Added tracing and API Gateway

---
+++ microservice-course-api-gateway/microservice-course-api-gateway/src/main/java/com/konvergion/personalfinance/microservicecourseapigateway/MicroserviceCourseApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.konvergion.personalfinance.microservicecourseapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCourseApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceCourseApiGatewayApplication.class, args);
+    }
+
+}
---
+++ microservice-course-api-gateway/microservice-course-api-gateway/src/test/java/com/konvergion/personalfinance/microservicecourseapigateway/MicroserviceCourseApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konvergion.personalfinance.microservicecourseapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCourseApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
